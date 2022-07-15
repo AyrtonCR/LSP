05a27d457ab81c206e33497233dbcf5ea2f654c4
duffa <duffa44@gmail.com>
2022-07-15 15:24:14 +1200

    add endpoints for single surfbreaks using id

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const monogoose = require("mongoose");
+const mongoose = require("mongoose");
 const SurfbreakModel = require("./models/SurfbreakModel");
 
 app.use(express.json());
@@ -8,6 +8,23 @@ app.use(express.json());
 app.get("/surfbreaks", async (request, response) => {
   const surfbreaks = await SurfbreakModel.find();
   return response.status(200).json(surfbreaks);
+});
+
+app.get("/surfbreaks/:id", async (request, response) => {
+  const { id } = request.params;
+
+  if (mongoose.Types.ObjectId.isValid(id)) {
+    const surfbreaks = await SurfbreakModel.findById(id);
+    if (surfbreaks) {
+      return response.status(200).send(surfbreaks);
+    } else {
+      return response
+        .status(404)
+        .send({ message: "The ID you are trying to access was not found" });
+    }
+  } else {
+    return response.status(400).send({ message: "The ID provided is Invalid" });
+  }
 });
 
 // app.get("/surfboards", async (request, response) => {
