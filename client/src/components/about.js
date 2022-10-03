789ce421bb73666525c04ee0563c41e6b3011a01
Ayrton Campbell-Reinheimer <ayrton.campbell-reinheimer@developersinstitute.co.nz>
2022-10-04 12:59:48 +1300

    add content for the about page and fix some sizing issues on the contact page, need nearly able to do full css revamp

--- client/src/components/about.js
+++ client/src/components/about.js
@@ -1,12 +1,55 @@
 import { Link } from "react-router-dom";
+import styles from "./about.module.css";
+import LowerNavBar from "./lowerNavBar";
+import MyFace from "../utils/myFaceCloser.png";
+import Southshore from "../utils/southshore.png";
 
 const About = () => {
   return (
     <div className="container">
-      <h1>About Page</h1>
-      <li>
-        <Link to="/">Main Link</Link>
-      </li>
+      <div className={styles.mainContainer}>
+        <h1 className={styles.mainTitle}>About Me</h1>
+        <div className={styles.mainGrid}>
+          <div className={styles.imageGrid}>
+            <img
+              className={styles.image1}
+              alt="ayrtons-face"
+              src={MyFace}
+            ></img>
+          </div>
+          <div className={styles.textGrid}>
+            <div className={styles.mainTextContainer}>
+              <p className={styles.mainText}>
+                Hi there, my name is Ayrton and I am a 26yo student in the last
+                stages of completing a Diploma in Web Development and Design. I
+                am based out of Christchurch and as you can probably tell I am
+                quite passionate about surfing!
+              </p>
+              <p className={styles.mainText}>
+                The goal of this project has been to use the different sills I
+                have learnt to build a functional site for people in
+                Christchurch who are looking to find some waves. Once the
+                project is complete I will do a full retrospective on the
+                process and find areas which can be improved. The plan is then
+                to rebuild the site a few times, possibly in different
+                languages, improving on the previous version each time!
+              </p>
+              <p className={styles.mainText}>
+                I am really enjoying the transition from trade work to Web
+                Development, and am finding the learning and designing process
+                stimulating and surprisingly fun.{" "}
+              </p>
+            </div>
+
+            <div className={styles.subTextContainer}>
+              <p className={styles.subText}>
+                Links to reference material will go here.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <LowerNavBar />
     </div>
   );
 };
--- client/src/components/contact.js
+++ client/src/components/contact.js
@@ -10,88 +10,90 @@ const Contact = () => {
         <div className={styles.mainContainer}>
           <h1 className={styles.MainTitle}>Feel free to get in touch ...</h1>
           <div className={styles.mainGrid}>
-            <div className={styles.formContainer}>
-              <form
-                action="https://formspree.io/f/xoqrdzrw"
-                method="POST"
-                className={styles.form}
-              >
-                <h3 className={styles.formTitle}>Contact Form</h3>
-                <div className={styles.formGroup}>
-                  <label for="name" class={styles.formLabel}>
-                    Name:
-                  </label>
+            <div className={styles.formContainerContainer}>
+              <div className={styles.formContainer}>
+                <form
+                  action="https://formspree.io/f/xoqrdzrw"
+                  method="POST"
+                  className={styles.form}
+                >
+                  <h3 className={styles.formTitle}>Contact Form</h3>
+                  <div className={styles.formGroup}>
+                    <label for="name" class={styles.formLabel}>
+                      Name:
+                    </label>
 
-                  <input
-                    id="name"
-                    name="Name"
-                    type="text"
-                    class={styles.formControl}
-                    placeholder="Enter your name here"
-                  />
-                </div>
+                    <input
+                      id="name"
+                      name="Name"
+                      type="text"
+                      class={styles.formControl}
+                      placeholder="Enter your name here"
+                    />
+                  </div>
 
-                <div className={styles.formGroup}>
-                  <label for="email" class={styles.formLabel}>
-                    Email:
-                  </label>
+                  <div className={styles.formGroup}>
+                    <label for="email" class={styles.formLabel}>
+                      Email:
+                    </label>
 
-                  <input
-                    id="email"
-                    name="Email"
-                    type="text"
-                    class={styles.formControl}
-                    placeholder="Enter your email address here"
-                  />
-                </div>
+                    <input
+                      id="email"
+                      name="Email"
+                      type="text"
+                      class={styles.formControl}
+                      placeholder="Enter your email address here"
+                    />
+                  </div>
 
-                <div className={styles.formGroup}>
-                  <label for="phone" class={styles.formLabel}>
-                    Mobile:
-                  </label>
+                  <div className={styles.formGroup}>
+                    <label for="phone" class={styles.formLabel}>
+                      Mobile:
+                    </label>
 
-                  <input
-                    id="phone"
-                    name="Phone"
-                    type="tel"
-                    class={styles.formControl}
-                    placeholder="Enter your mobile number here"
-                  />
-                </div>
+                    <input
+                      id="phone"
+                      name="Phone"
+                      type="tel"
+                      class={styles.formControl}
+                      placeholder="Enter your mobile number here"
+                    />
+                  </div>
 
-                <div className={styles.formGroup}>
-                  <label for="message" class={styles.formLabel}>
-                    Message:
-                  </label>
+                  <div className={styles.formGroup}>
+                    <label for="message" class={styles.formLabel}>
+                      Message:
+                    </label>
 
-                  <textarea
-                    id="message"
-                    name="Message"
-                    class={styles.formControl}
-                    placeholder="Enter your message here"
-                  />
-                </div>
-                <div className={styles.formButtonContainer}>
-                  <button type="submit" className={styles.formButton}>
-                    Submit
-                  </button>
-                </div>
-              </form>
-              <div className={styles.contactCardContainer}>
-                <h3 className={styles.formTitle}>My Contact Info</h3>
-                <div className={styles.contactCard}>
-                  <p className={styles.contactCardContent}>
-                    Email: email@notgmail.com
-                  </p>
-                  <p className={styles.contactCardContent}>
-                    Mobile: (+64) 21 234 5678
-                  </p>
-                  <p className={styles.contactCardContent}>
-                    Work: (03) 322-2233
-                  </p>
-                  <p className={styles.contactCardContent}>
-                    Address: 38 Surfer Dude Lane
-                  </p>
+                    <textarea
+                      id="message"
+                      name="Message"
+                      class={styles.formControl}
+                      placeholder="Enter your message here"
+                    />
+                  </div>
+                  <div className={styles.formButtonContainer}>
+                    <button type="submit" className={styles.formButton}>
+                      Submit
+                    </button>
+                  </div>
+                </form>
+                <div className={styles.contactCardContainer}>
+                  <h3 className={styles.formTitle}>My Contact Info</h3>
+                  <div className={styles.contactCard}>
+                    <p className={styles.contactCardContent}>
+                      Email: email@notgmail.com
+                    </p>
+                    <p className={styles.contactCardContent}>
+                      Mobile: (+64) 21 234 5678
+                    </p>
+                    <p className={styles.contactCardContent}>
+                      Work: (03) 322-2233
+                    </p>
+                    <p className={styles.contactCardContent}>
+                      Address: 38 Surfer Dude Lane
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
