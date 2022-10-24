c4bd858c568f90f90ced1e0ee3673528f2b2ed9b
Ayrton Campbell-Reinheimer <ayrton.campbell-reinheimer@developersinstitute.co.nz>
2022-10-24 20:19:42 +1300

    complete first basic css restyle

--- client/src/components/Navbar/MobileNavigation.js
+++ client/src/components/Navbar/MobileNavigation.js
@@ -1,8 +1,11 @@
-import styles from "./NavBar.module.css";
-import NavLinks from "./NavLinks";
+import { Link } from "react-router-dom";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { AiFillCloseCircle } from "react-icons/ai";
 import { useState } from "react";
+import styles from "./NavBar.module.css";
+import NavLinks from "./NavLinks";
+import Main from "../main";
+import Logo from "../../utils/surfing-board.jpeg";
 
 const MobileNavigation = () => {
   const [open, setOpen] = useState(false);
@@ -18,7 +21,7 @@ const MobileNavigation = () => {
   const closeIcon = (
     <AiFillCloseCircle
       className={styles.Hamburger}
-      size="40px"
+      size="1.8rem"
       onClick={() => setOpen(!open)}
     />
   );
@@ -28,6 +31,13 @@ const MobileNavigation = () => {
     <nav className={styles.MobileNavigation}>
       {open ? closeIcon : hamburgerIcon}
       {open && <NavLinks isMobile={true} closeMobileMenu />}
+      <Link to="/" className={styles.headerTitle} element={<Main />}>
+        <img
+          src={Logo}
+          alt="surfboard_logo"
+          className={styles.mobileLogo}
+        ></img>
+      </Link>
     </nav>
   );
 };
