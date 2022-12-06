29f358795df26f67b7826aef4388cd25ae1c0eb2
duffa <duffa44@gmail.com>
2022-12-06 14:52:53 +1300

    Deployment working, will create development branches that have connection set up for localhost

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const port = process.env.PORT || 5001;
 const app = require("./app");
 const mongoose = require("mongoose");
 
+// This is the connection to the hosted Mongo Atlas Database //
 mongoose.connect("mongodb+srv://auth-user:auth-password@lsp.slfpy.mongodb.net/lsp?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
