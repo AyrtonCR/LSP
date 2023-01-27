d4a28ebf8133446b5746ab34d8b09e55e847dbcc
Ayrton Campbell <ayrton.campbell-reinheimer@developersinstitute.ac.nz>
2023-01-28 12:02:28 +1300

    add browserrouter to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import Auth0ProviderWithHistory from "./components/Auth0ProviderWithHistory";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.render(
