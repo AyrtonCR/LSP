8c9f5ddb2a4188509a8bf9ad803e0ba53b6003c1
Ayrton Campbell <ayrton.campbell-reinheimer@developersinstitute.ac.nz>
2023-01-31 10:47:06 +1300

    looking good just needs mobile friendly

--- client/src/components/surfboard.js
+++ client/src/components/surfboard.js
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import styles from "./surfboard.module.css";
 import LowerNavBar from "./lowerNavBar";
 import React, { useState, useEffect } from "react";
+import Wave from "../utils/wave3.png";
 
 const API_URL = process.env.REACT_APP_API_URL;
 
@@ -20,8 +21,12 @@ const Surfboard = () => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.surfboardGridContainer}>
+      <div className={styles.spaceSaver}></div>
+      <div className={styles.titleAndImage}>
         <h2 className={styles.surfboardTitle}>Surfboards</h2>
+        <img className="mini-image" src={Wave} alt="wave"></img>
+      </div>
+      <div className={styles.surfboardGridContainer}>
         <div className={styles.surfboardsBlurbContainer}>
           <h4 className={styles.surfboardsBlurb}>
             Surfboards can come in many different shapes and sizes. From long to
@@ -62,16 +67,33 @@ const Surfboard = () => {
       </div>
       <div className={styles.lowerBlurbContainerContainer}>
         <div className={styles.lowerBlurbContainer}>
-          <h4 className={styles.surfboardsBlurb}>
-            This is a just a brief indication of what can these types of
-            surfboards can do.{" "}
+          <h4 className={styles.lowerBlurb1}>
+            <em>
+              There are many different shapes, build materials and setups for
+              each type of board that can completely change the way a board
+              functions, allowing it to often be either more easy and fun, or
+              more tricky and rippable.{" "}
+            </em>
           </h4>
-          <h4 className={styles.surfboardsBlurb}>
-            There are many different shapes, build materials and setups for each
-            type of board that can completely change the way a board functions,
-            allowing it to often be either more easy and fun, or more tricky and
-            rippable.{" "}
-          </h4>
+          <div className={styles.lowerBlurb2Container}>
+            <h4 className={styles.lowerBlurb2}>
+              <em className={styles.lowerBlurb1Content}>
+                This is a just a brief indication of what these types of
+                surfboards can do.{" "}
+              </em>
+            </h4>
+            <h4 className={styles.lowerBlurb2}>
+              <em className={styles.lowerBlurb2Content}>
+                Images are from a great Christchurch shaper <br></br>
+                <a
+                  className={styles.sadhanaLink}
+                  href="https://sadhanasurfboards.co.nz/"
+                >
+                  @Sadhana Surfboards
+                </a>
+              </em>
+            </h4>
+          </div>
         </div>
       </div>
       <LowerNavBar />
