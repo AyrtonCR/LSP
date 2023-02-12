2a2d3d20a2f1c759d4c338ae9dbe6d89e5593087
Ayrton Campbell <ayrton.campbell-reinheimer@developersinstitute.ac.nz>
2023-02-12 13:26:15 +1300

    big rework of forecasts page, looking much better minor styling left to go

--- client/src/components/forecasts.js
+++ client/src/components/forecasts.js
@@ -22,34 +22,35 @@ const Forecasts = () => {
   return (
     <div className={forecastStyles.container}>
       <div className={forecastStyles.spaceSaver}></div>
-      <motion.div
-        className={forecastStyles.titleContainer}
-        initial={{ scale: 1, opacity: 0, x: -300 }}
-        animate={{ x: 0, opacity: 1 }}
-        transition={{ duration: 1.4 }}
-      >
-        <motion.h2
-          className={forecastStyles.title}
-          initial={{ opacity: 0.3 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 1 }}
+      <div className={forecastStyles.titleContainerContainer}>
+        <motion.div
+          className={forecastStyles.titleContainer}
+          initial={{ scale: 1, opacity: 0, x: -300 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 1.4 }}
         >
-          Forecast
-        </motion.h2>
-        <motion.img
-          initial={{ opacity: 0, x: 200 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{
-            x: { duration: 1 },
-            opacity: { duration: 1.2 },
-            delay: 0.5,
-          }}
-          className={forecastStyles.image}
-          src={Wave}
-          alt="wave"
-        ></motion.img>
-      </motion.div>
-
+          <motion.h2
+            className={forecastStyles.title}
+            initial={{ opacity: 0.3 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+          >
+            Forecast
+          </motion.h2>
+          <motion.img
+            initial={{ opacity: 0, x: 200 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{
+              x: { duration: 1 },
+              opacity: { duration: 1.2 },
+              delay: 0.5,
+            }}
+            className={forecastStyles.image}
+            src={Wave}
+            alt="wave"
+          ></motion.img>
+        </motion.div>
+      </div>
       <div className={forecastStyles.forecastsContainer}>
         <div className={forecastStyles.forecastsGrid}>
           <h4 className={forecastStyles.mainBlurb}>
@@ -83,47 +84,60 @@ const Forecasts = () => {
                     </div>
                     <div className={forecastStyles.mainGridPart2}>
                       <div className={forecastStyles.forecastsItemGridPart1}>
+                        <p className={forecastStyles.breakBlurb}>
+                          {forecast.forecast_info}
+                        </p>
+                      </div>
+                      <div className={forecastStyles.forecastsItemGridPart2}>
                         <img
                           alt="forecasts"
                           className={forecastStyles.forecastsImage}
                           src={forecast.forecasts_image}
                         ></img>
                       </div>
-                      <div className={forecastStyles.forecastsItemGridPart2}>
-                        <a
-                          href={forecast.forecasts_info_1}
-                          className={forecastStyles.forecastButton}
-                        >
-                          <button className={forecastStyles.nestedButton}>
-                            <strong>Magic Seaweed Report</strong>
-                          </button>
-                        </a>
-
-                        <a
-                          href={forecast.forecasts_info_2}
-                          className={forecastStyles.forecastButton}
-                        >
-                          <button className={forecastStyles.nestedButton}>
-                            <strong>Surf Forecast Report</strong>
-                          </button>
-                        </a>
-                        <a
-                          href={forecast.forecasts_cam_1}
-                          className={forecastStyles.forecastButton}
-                        >
-                          <button className={forecastStyles.nestedButton}>
-                            <strong>Surf Cam (1)</strong>
-                          </button>
-                        </a>
-
-                        <a
-                          href={forecast.forecasts_cam_2}
-                          className={forecastStyles.forecastButton}
-                        >
-                          <button className={forecastStyles.nestedButton}>
-                            <strong>Surf Cam (2)</strong>
-                          </button>
-                        </a>
+                      <div className={forecastStyles.forecastsItemGridPart3}>
+                        {" "}
+                        <p className={forecastStyles.reportName}>
+                          Magic Seaweed Report:
+                          <a
+                            href={forecast.forecasts_info_1}
+                            className={forecastStyles.forecastButton}
+                          >
+                            <button className={forecastStyles.nestedButton}>
+                              <strong>Click Here</strong>
+                            </button>
+                          </a>
+                        </p>
+                        <p className={forecastStyles.reportName}>
+                          Surf Forecast Report:
+                          <a
+                            href={forecast.forecasts_info_2}
+                            className={forecastStyles.forecastButton}
+                          >
+                            <button className={forecastStyles.nestedButton}>
+                              <strong>Click Here</strong>
+                            </button>
+                          </a>
+                        </p>
+                        <p className={forecastStyles.reportName}>
+                          Surf Cams:
+                          <a
+                            href={forecast.forecasts_cam_1}
+                            className={forecastStyles.forecastButton}
+                          >
+                            <button className={forecastStyles.nestedButton}>
+                              <strong>Cam (1)</strong>
+                            </button>
+                          </a>
+                          <a
+                            href={forecast.forecasts_cam_2}
+                            className={forecastStyles.forecastButton}
+                          >
+                            <button className={forecastStyles.nestedButton}>
+                              <strong>Cam (2)</strong>
+                            </button>
+                          </a>
+                        </p>
                       </div>
                     </div>
                   </li>
